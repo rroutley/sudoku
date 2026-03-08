b93ee273361bbb80d01224161c24acfee86b2726
Richard Routley <richardroutley@gmail.com>
2026-03-08 11:04:02 +0800

    .net 10

--- Test/Program.cs
+++ Test/Program.cs
@@ -20,28 +20,28 @@ namespace Test
 
                 //completeBoard.Print(Console.Out);
 
-                newPuzzle = gen.BuildPuzzle(completeBoard, new Generator.GeneratorOptions { Strategies = new[] { new SingleCandidateStrategy() } });
+                newPuzzle = gen.BuildPuzzle(completeBoard, new Generator.GeneratorOptions { Strategies = Generator.ToughStrategies });
                 //newPuzzle.Print(Console.Out);
 
             //} while (newPuzzle.CellsFilled > 20);
 
-            Console.WriteLine(newPuzzle);
+            // Console.WriteLine(newPuzzle);
 
-            var solver = new Solver();
-            solver.TraceOutput = Console.Out;
-            var t = Board.Load(newPuzzle.ToString());
-            var success = solver.Execute(t);
+            // var solver = new Solver();
+            // solver.TraceOutput = Console.Out;
+            // var t = Board.Load(newPuzzle.ToString());
+            // var success = solver.Execute(t);
             
-            Console.WriteLine(newPuzzle);
+            // Console.WriteLine(newPuzzle);
 
           //  string puzzle;
 
-            //var x = File.ReadLines(@"..\..\top95.txt");
+            var x = File.ReadLines(@"top95.txt");
 
-            //int number = 0;
-            ////foreach(var puzzle in x)
-            //while(true)
-            //{
+            int number = 0;
+            foreach(var puzzle in x)
+            // while(true)
+            {
             //    number++;
             //    //Console.WriteLine("Enter Grid number");
             //    //var input = Console.ReadLine();
@@ -59,27 +59,27 @@ namespace Test
             //    Console.WriteLine("Grid {0:00}", number);
             //    var puzzle = LoadGrid(number);
 
-            //    Board b = Board.Load(puzzle);
+               Board b = Board.Load(puzzle);
 
-            //    var solver = new Solver();
-            //   // solver.TraceOutput = Console.Out;
-            //    b.Print(solver.TraceOutput);
+               var solver = new Solver();
+            //   solver.TraceOutput = Console.Out;
+               b.Print(solver.TraceOutput);
 
-            //    var success = solver.Execute(b);
-            //    if (!success)
-            //    {
-            //        b.Print(Console.Out);
-            //    }
+               var success = solver.Execute(b);
+               if (!success)
+               {
+                   b.Print(Console.Out);
+               }
 
 
-            //}
+            }
 
-            //Console.WriteLine("Naked Pair used {0} times", NakedCandidatesStrategy.Count);
-            //if (BruteForceStrategy.Count > 0)
-            //{
+            // Console.WriteLine("Naked Pair used {0} times", NakedCandidatesStrategy.Count);
+            // if (BruteForceStrategy.Count > 0)
+            // {
             //    Console.WriteLine("Brute Force used {0} times", BruteForceStrategy.Count);
-            //}
-            Console.ReadLine();
+            // }
+            // Console.ReadLine();
         }
 
         private static string LoadGrid(int number)
@@ -88,7 +88,7 @@ namespace Test
             var grid = string.Format("Grid {0:00}", number);
 
             int line = 0;
-            var lines = File.ReadAllLines(@"..\..\p096_sudoku.txt");
+            var lines = File.ReadAllLines(@"p096_sudoku.txt");
             for (; lines[line] != grid; line++) ;
 
             line++;
